071a360ba87b4046da9b31bda07d10ad57e36777
rhostem <syoung.j@gmail.com>
2018-11-22 01:17:59 +0900

    convert to camelcase

--- src/converter/case.js
+++ src/converter/case.js
@@ -1,4 +1,46 @@
-export function isCamelCase(s = '') {
-  const str = s.toString()
-  return /^[a-z]+([A-Z][a-z]{1,})+$/.test(str)
+import * as R from 'ramda'
+
+export function isCamelCase(str = '') {
+  return /^[a-z][a-z0-9]+([A-Z][a-z0-9]{1,})+$/.test(str)
+}
+
+export function splitCamelCase(str) {
+  if (isCamelCase(str)) {
+    return str.split(/\B(?=[A-Z][a-z]+)/g).map(R.toLower)
+  } else {
+    return [str]
+  }
+}
+
+/**
+ * prepare for case  conversion
+ * all items should be lower case and
+ * @param {*} words
+ */
+function prepareWordsForConversion(words = []) {
+  if (Array.isArray(words)) {
+    return R.pipe(
+      R.filter(w => R.not(R.isEmpty(w))),
+      R.filter(w => typeof w === 'string')
+    )(words)
+  } else {
+    console.error('words shoud be an array')
+    return words
+  }
+}
+
+export function convertToCamel(words = []) {
+  return R.pipe(
+    prepareWordsForConversion,
+    wordArr => {
+      return wordArr.map((word, index) => {
+        if (index !== 0) {
+          return `${word[0].toUpperCase()}${word.slice(1, word.length)}`
+        } else {
+          return word
+        }
+      })
+    },
+    R.join('')
+  )(words)
 }
--- src/converter/case.test.js
+++ src/converter/case.test.js
@@ -1,20 +1,46 @@
-import { isCamelCase } from './case'
+import { isCamelCase, splitCamelCase, convertToCamel } from './case'
+import * as R from 'ramda'
 
-const CAMEL = 'reactApp'
-const SNAKE = 'react_app'
-const CONSTANT = 'REACT_APP'
-const PASCAL = 'ReactApp'
-const KEBAB = 'react-app'
+const words = ['create', 'react', 'app']
+const CAMEL = 'createReactApp'
+const PASCAL = 'CreateReactApp'
+const CONSTANT = 'CREATE_REACT_APP'
+const KEBAB = 'create-react-app'
+const SNAKE = 'create_react_app'
 
-describe('isCamelCase 함수 테스트', () => {
-  test('camel 케이스', () => {
+const wordsWithNum = ['c1reate', 'react2', 'app3']
+const CAMEL_WITH_NUM = 'c1reateReact2App3'
+const PASCAL_WITH_NUM = 'C1reateReact2App3'
+const CONSTANT_WITH_NUM = 'C1REATE_REACT2_APP3'
+const KEBAB_WITH_NUM = 'c1reate-react2-app3'
+const SNAKE_WITH_NUM = 'c1reate_react2_app3'
+
+describe('camel case', () => {
+  test('is camel case', () => {
     expect(isCamelCase(CAMEL)).toBe(true)
+    expect(isCamelCase(CAMEL_WITH_NUM)).toBe(true)
   })
 
-  test('camel 케이스가 아니다', () => {
+  test('is not camel case', () => {
     expect(isCamelCase(SNAKE)).toBe(false)
-    expect(isCamelCase(CONSTANT)).toBe(false)
+    expect(isCamelCase(SNAKE_WITH_NUM)).toBe(false)
     expect(isCamelCase(PASCAL)).toBe(false)
+    expect(isCamelCase(PASCAL_WITH_NUM)).toBe(false)
+    expect(isCamelCase(CONSTANT)).toBe(false)
+    expect(isCamelCase(CONSTANT_WITH_NUM)).toBe(false)
     expect(isCamelCase(KEBAB)).toBe(false)
+    expect(isCamelCase(KEBAB_WITH_NUM)).toBe(false)
+    expect(isCamelCase(SNAKE)).toBe(false)
+    expect(isCamelCase(SNAKE_WITH_NUM)).toBe(false)
+  })
+
+  test('separate word into array of lowercase', () => {
+    expect(R.equals(words, splitCamelCase(CAMEL))).toBe(true)
+    expect(R.equals(wordsWithNum, splitCamelCase(CAMEL_WITH_NUM))).toBe(true)
+  })
+
+  test('convert to camelcase', () => {
+    expect(convertToCamel(words)).toBe(CAMEL)
+    expect(convertToCamel(wordsWithNum)).toBe(CAMEL_WITH_NUM)
   })
 })
